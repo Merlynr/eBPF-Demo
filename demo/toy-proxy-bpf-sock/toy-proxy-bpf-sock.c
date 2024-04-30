d2e138306c9bddb1d3ec3588159ff0ec84ed4e39
Merlynr <lcq1013962426@gmail.com>
2024-04-30 10:01:15 +0000

    add tcp-filter failed [https://arthurchiao.art/blog/cracking-k8s-node-proxy/#8-implementation-5-proxy-via-socket-level-ebpf]

--- demo/toy-proxy-bpf-sock/toy-proxy-bpf-sock.c
+++ demo/toy-proxy-bpf-sock/toy-proxy-bpf-sock.c
@@ -5,5 +5,28 @@
 # Created Time: Tue 30 Apr 2024 09:12:02 AM CST
 #########################################################################*/
 
-#include<stdio.h>
+#include <linux/bpf.h>
 
+#ifndef __section
+#define __section(NAME) \
+	__attribute__((section(NAME), used))
+#endif
+
+static int
+__sock4_xlate_fwd(struct bpf_sock_addr *ctx) {
+	const __be32 cluster_ip = 0x846F070A; // 10.7.111.132
+	const __be32 pod_ip = 0x0529050A;     // 10.5.41.5
+
+	if (ctx->user_ip4 != cluster_ip) {
+		return 0;
+	}
+
+	ctx->user_ip4 = pod_ip;
+	return 0;
+}
+
+__section("connect4")
+int sock4_connect(struct bpf_sock_addr *ctx) {
+	__sock4_xlate_fwd(ctx);
+	return SYS_PROCEED;
+}
